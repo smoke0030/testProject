a76b98f48a4c94cafb0d53e5434f64039c2192aa
Sergey Vidishev <ft.priest@gmail.com>
2024-08-05 19:45:00 +0300

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Сергей on 05.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
